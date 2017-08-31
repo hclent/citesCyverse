52a5e3b7aa6b70feea123b370384d9455f1e1ea2
hclent <hclent@email.arizona.edu>
2017-08-30 17:05:57 -0700

    Updated path to static

--- fgraph2json.py
+++ fgraph2json.py
@@ -25,7 +25,7 @@ def embedding_json(results, query, k, top_n):
     links = json_out["links"]
     print_json = {"nodes": ordered_nodes, "links": links}
 
-    save_path = "/Users/heather/Desktop/citesCyverse/web" #/flask/static/fgraphs
+    save_path = "/Users/heather/Desktop/citesCyverse/static/fgraphs"
     filename =  'fgraph_' + str(query) + '_' + str(k) + '_' + str(top_n) + '.json'
 
     completeName = os.path.join(save_path, filename)  # with the query for a name
