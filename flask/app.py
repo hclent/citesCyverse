f36fcf9bc2b294f48e7bd12b9076229511db6d8e
hclent <hclent1@gmail.com>
2017-09-13 18:58:39 -0700

    updated default wordcloud

--- flask/app.py
+++ flask/app.py
@@ -22,7 +22,7 @@ logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S
 
 #load model once globally
 logging.info("* LOADING FASTTEXT MODEL... THIS TAKES A WHILE...")
-model = load_model("17kmodel.vec")
+model = load_model("17kmodel.vec") #"cyverseModel.vec
 logging.info("!!! DONE LOADING FASTTEXT MODEL :')")
 
 #Form for Topic Modeling
@@ -61,8 +61,8 @@ def cyJournals():
                            unique_journals=unique_journals, s_year=s_year, e_year=e_year)
 
 
-#TODO: add form and button to generate wordcloud for a given keyword
-#TODO: fix caching
+#TODO: make a new default
+#TODO: always say what the wordcloud is
 #Maybe use TF-IDF to filter words instead of most frequent?
 @app.route("/cy-wordcloud/", methods=["GET", "POST"])
 def cyWordcloud():
@@ -72,6 +72,8 @@ def cyWordcloud():
         logging.info(searchWord)
         #check for file
 
+        message = "Displaying cloud for words in citing papers most relating to: " + str(searchWord)
+
         try:
             path_to_wordcloud = "/home/hclent/repos/citesCyverse/flask/static/wordclouds"
             filename = searchWord + ".json"
@@ -79,7 +81,7 @@ def cyWordcloud():
 
             with open(path) as f:
                 wordcloud_data = json.load(f)
-                return render_template("wordcloud.html", wordcloud_data=wordcloud_data)
+                return render_template("wordcloud.html", wordcloud_data=wordcloud_data, message=message)
 
         except Exception as e:
             logging.info("no file for this one...")
@@ -91,22 +93,23 @@ def cyWordcloud():
                 wordcloud_words = filter_by_embeddings(searchWord, nesDict)
                 logging.info(wordcloud_words)
                 wordcloud_data = wordcloud(searchWord, nesDict, wordcloud_words)
-                #cache file
-                logging.info(type(wordcloud_data))
-                return render_template("wordcloud.html", wordcloud_data=wordcloud_data)
+                return render_template("wordcloud.html", wordcloud_data=wordcloud_data, message=message)
             except Exception as e2:
                 return("So sorry! The word " + str(searchWord) + " has no vector, so we can't generate a word cloud. Please refresh the page and try again!")
 
     else:
-        wordcloud_data = [{"text": "predisposed", "size": 10}, {"text": "undiagnosed", "size": 10}, {"text": "fatal", "size": 13}, {"text": "manifestation", "size": 16}, {"text": "neurological", "size": 10}, {"text": "cure", "size": 25}, {"text": "disease.", "size": 10}, {"text": "symptomatology", "size": 10}, {"text": "development", "size": 3205}, {"text": "misdiagnosed", "size": 10}, {"text": "autoimmune", "size": 31}, {"text": "diagnose", "size": 44}, {"text": "disease", "size": 692}, {"text": "antecedent", "size": 10}, {"text": "disorder", "size": 72}, {"text": "asymptomatic", "size": 10}, {"text": "chronic", "size": 23}, {"text": "severity", "size": 84}, {"text": "diabetes", "size": 14}, {"text": "illness", "size": 10}, {"text": "pathotype", "size": 10}, {"text": "progression", "size": 59}, {"text": "superinfection", "size": 10}, {"text": "causative", "size": 15}, {"text": "cardiovascular", "size": 17}, {"text": "deteriorating", "size": 10}, {"text": "risk", "size": 301}, {"text": "pathological", "size": 20}, {"text": "deterioration", "size": 10}, {"text": "autoimmunity", "size": 20}, {"text": "neurodegenerative", "size": 10}, {"text": "devastating", "size": 10}, {"text": "preventable", "size": 10}, {"text": "neurologic", "size": 10}, {"text": "disability", "size": 10}, {"text": "dementia", "size": 10}, {"text": "management", "size": 2631}, {"text": "recurrence", "size": 10}, {"text": "suffering", "size": 10}, {"text": "causally", "size": 10}, {"text": "severe", "size": 100}, {"text": "clinically", "size": 10}, {"text": "diseased", "size": 26}, {"text": "pathogenesis", "size": 10}, {"text": "recurrent", "size": 25}, {"text": "patient", "size": 92}, {"text": "etiology", "size": 10}, {"text": "diagnosis", "size": 68}, {"text": "outcome", "size": 353}, {"text": "pathology", "size": 47}, {"text": "recurrently", "size": 10}, {"text": "malignancy", "size": 10}, {"text": "prevention", "size": 27}, {"text": "sclerosis", "size": 10}, {"text": "treat", "size": 261}, {"text": "systemic", "size": 76}, {"text": "symptomatic", "size": 10}, {"text": "clinical", "size": 146}, {"text": "disease-", "size": 10}, {"text": "asymptomatically", "size": 10}, {"text": "complication", "size": 30}, {"text": "pathophysiology", "size": 10}, {"text": "neurologically", "size": 10}, {"text": "susceptibility", "size": 70}, {"text": "progressive", "size": 31}, {"text": "predisposition", "size": 10}, {"text": "early-onset", "size": 10}, {"text": "threatening", "size": 10}, {"text": "ulcerative", "size": 10}, {"text": "late-onset", "size": 10}, {"text": "incidence", "size": 37}, {"text": "disease-resistance", "size": 10}, {"text": "early-stage", "size": 10}, {"text": "prevalence", "size": 54}, {"text": "prognosis", "size": 10}, {"text": "pathogenic", "size": 38}]
-        wordcloud_data = json.dumps(wordcloud_data)
-        #wordcloud_data = [{"text": "geochemical", "size": 3}, {"text": "groundwater", "size": 15}, {"text": "sunlit", "size": 7}, {"text": "seafloor", "size": 28}, {"text": "chemolithoautotrophic", "size": 1}, {"text": "metacommunity", "size": 3}, {"text": "inhabit", "size": 28}, {"text": "shallow-water", "size": 1}, {"text": "plankton", "size": 32}, {"text": "lake", "size": 99}, {"text": "marine", "size": 339}, {"text": "biogeographical", "size": 39}, {"text": "sediment", "size": 45}, {"text": "microclimate", "size": 20}, {"text": "pond", "size": 85}, {"text": "rhizosphere", "size": 9}, {"text": "phytoplankton", "size": 94}, {"text": "seawater.", "size": 1}, {"text": "heterotrophically", "size": 1}, {"text": "hydrothermally", "size": 1}, {"text": "geothermal", "size": 1}, {"text": "estuarine", "size": 2}, {"text": "low-water", "size": 1}, {"text": "subsurface", "size": 16}, {"text": "ecological", "size": 653}, {"text": "reef", "size": 13}, {"text": "alpine", "size": 28}, {"text": "methane", "size": 2}, {"text": "metagenome", "size": 340}, {"text": "mesopelagic", "size": 25}, {"text": "submarine", "size": 4}, {"text": "biome", "size": 53}, {"text": "pelagic", "size": 11}, {"text": "agroecosystem", "size": 10}, {"text": "zooplankton", "size": 45}, {"text": "microbial", "size": 601}, {"text": "volcanic", "size": 11}, {"text": "agro-ecosystem", "size": 11}, {"text": "high-water", "size": 1}, {"text": "plume", "size": 12}, {"text": "biosphere", "size": 22}, {"text": "deep-sea", "size": 10}, {"text": "habitation", "size": 2}, {"text": "watershed", "size": 55}, {"text": "anoxygenic", "size": 2}, {"text": "wastewater", "size": 2}, {"text": "mycoheterotrophic", "size": 1}, {"text": "community", "size": 4098}, {"text": "soil", "size": 822}, {"text": "ecosystem", "size": 692}, {"text": "bedrock", "size": 7}, {"text": "anthropogenic", "size": 98}, {"text": "arctic", "size": 23}, {"text": "ocean", "size": 318}, {"text": "fauna", "size": 20}, {"text": "mariner", "size": 4}, {"text": "palaeoclimate", "size": 1}, {"text": "biosphere.", "size": 1}, {"text": "hypersaline", "size": 13}, {"text": "bathypelagic", "size": 8}, {"text": "ice-water", "size": 1}, {"text": "aquifer", "size": 2}, {"text": "oceanography", "size": 25}, {"text": "virioplankton", "size": 1}, {"text": "headwater", "size": 4}, {"text": "upwelled", "size": 1}, {"text": "habitat", "size": 562}, {"text": "chimney", "size": 3}, {"text": "archipelago", "size": 61}, {"text": "freshwater", "size": 44}, {"text": "anoxic", "size": 6}, {"text": "metagenomic", "size": 379}, {"text": "euphotic", "size": 2}, {"text": "ecosystem-level", "size": 1}, {"text": "geochemistry", "size": 6}, {"text": "metaproteome", "size": 4}, {"text": "mangrove", "size": 5}, {"text": "surface-water", "size": 1}, {"text": "grassland", "size": 58}, {"text": "ecosystem.", "size": 2}, {"text": "lagoon", "size": 72}, {"text": "glacier", "size": 28}, {"text": "atmospheric", "size": 84}, {"text": "terrain", "size": 75}, {"text": "subseafloor", "size": 2}, {"text": "epipelagic", "size": 38}, {"text": "shallow", "size": 37}, {"text": "waterfall", "size": 1}, {"text": "estuary", "size": 9}, {"text": "sea-ice", "size": 1}, {"text": "subtropical", "size": 34}, {"text": "inhabitant", "size": 5}, {"text": "aquatic", "size": 94}, {"text": "mud", "size": 23}, {"text": "seawater", "size": 55}, {"text": "salinity", "size": 60}, {"text": "biogeographic", "size": 55}, {"text": "ecologic", "size": 3}, {"text": "gyre", "size": 3}, {"text": "solar", "size": 102}, {"text": "picoplankton", "size": 24}, {"text": "deep-ocean", "size": 2}, {"text": "bacterioplankton", "size": 2}, {"text": "sewage", "size": 7}, {"text": "brackish", "size": 4}, {"text": "underwater", "size": 6}, {"text": "heterotroph", "size": 2}, {"text": "limnic", "size": 2}, {"text": "coastal", "size": 108}, {"text": "biodiversity", "size": 1035}, {"text": "ecology", "size": 801}, {"text": "coast", "size": 68}, {"text": "riverine", "size": 4}, {"text": "pelagibacter", "size": 2}, {"text": "savannah", "size": 3}, {"text": "river", "size": 147}, {"text": "habitats.", "size": 1}, {"text": "mixotrophic", "size": 1}, {"text": "metagenomics", "size": 2}, {"text": "planktonic", "size": 8}, {"text": "offshore", "size": 1}, {"text": "oceanic", "size": 54}, {"text": "biogeochemical", "size": 23}, {"text": "assemblage", "size": 178}, {"text": "muddy", "size": 1}, {"text": "heterotrophy", "size": 1}, {"text": "deepwater", "size": 1}, {"text": "temperate", "size": 228}, {"text": "microclimatic", "size": 3}, {"text": "borehole", "size": 9}, {"text": "planet", "size": 57}, {"text": "shore", "size": 6}, {"text": "cosmopolitan", "size": 8}, {"text": "mountainous", "size": 14}, {"text": "photic", "size": 9}, {"text": "deep-water", "size": 10}, {"text": "oxic", "size": 2}, {"text": "heterotrophic", "size": 20}, {"text": "terrestrial", "size": 108}, {"text": "high-latitude", "size": 1}, {"text": "biogeochemistry", "size": 15}, {"text": "wetland", "size": 28}, {"text": "autotrophic", "size": 4}, {"text": "hydrothermal", "size": 12}, {"text": "arid", "size": 47}, {"text": "microhabitat", "size": 2}, {"text": "open-ocean", "size": 1}, {"text": "intertidal", "size": 4}, {"text": "horizon", "size": 27}, {"text": "metaproteomic", "size": 6}, {"text": "pelagiphage", "size": 1}, {"text": "oligotrophic", "size": 3}, {"text": "picophytoplankton", "size": 2}, {"text": "photoautotrophic", "size": 2}, {"text": "sedimentary", "size": 5}, {"text": "depths", "size": 66}, {"text": "cyanobacterial", "size": 15}, {"text": "upwelling", "size": 8}, {"text": "oceanographic", "size": 12}, {"text": "basaltic", "size": 4}, {"text": "ephemeral", "size": 12}]
-        #wordcloud_data = [{"size": 10, "text": "carcinogenesis"}, {"size": 10, "text": "invasively"}, {"size": 10, "text": "malignancy"}, {"size": 10, "text": "cancer-type"}, {"size": 92, "text": "patient"}, {"size": 37, "text": "incidence"}, {"size": 10, "text": "chemotherapeutic"}, {"size": 11, "text": "metastatic"}, {"size": 10, "text": "pancreatic"}, {"size": 10, "text": "anti-metastasis"}, {"size": 10, "text": "prostate"}, {"size": 10, "text": "prognosis"}, {"size": 10, "text": "recurrence"}, {"size": 10, "text": "colorectal"}, {"size": 143, "text": "aggressive"}, {"size": 10, "text": "small-cell"}, {"size": 10, "text": "melanoma"}, {"size": 3222, "text": "cell"}, {"size": 10, "text": "colon"}, {"size": 10, "text": "carcinoma"}, {"size": 10, "text": "malignant"}, {"size": 10, "text": "gastric"}, {"size": 10, "text": "adenocarcinoma"}, {"size": 10, "text": "invasiveness"}, {"size": 24, "text": "tumor"}, {"size": 12, "text": "metastasis"}, {"size": 266, "text": "cancer"}, {"size": 10, "text": "cervical"}, {"size": 127, "text": "invasive"}, {"size": 10, "text": "tumorigenesis"}, {"size": 10, "text": "stromal"}, {"size": 10, "text": "abreast"}, {"size": 20, "text": "lung"}, {"size": 10, "text": "cell-line"}, {"size": 59, "text": "progression"}, {"size": 10, "text": "bladder"}, {"size": 10, "text": "breast-cancer"}, {"size": 10, "text": "men"}, {"size": 42, "text": "breast"}, {"size": 80, "text": "oral"}, {"size": 10, "text": "hepatocellular"}]
-        #wordcloud_data = [{"size": 24, "text": "optics"}, {"size": 106, "text": "photograph"}, {"size": 10, "text": "semi-automatically"}, {"size": 261, "text": "optical"}, {"size": 66, "text": "zoom"}, {"size": 18, "text": "time-lapse"}, {"size": 10, "text": "cropped"}, {"size": 10, "text": "gray-scale"}, {"size": 873, "text": "digital"}, {"size": 20, "text": "semi-automatic"}, {"size": 10, "text": "fiducial"}, {"size": 87, "text": "laser"}, {"size": 15, "text": "top-view"}, {"size": 11, "text": "multi-view"}, {"size": 4039, "text": "image"}, {"size": 10, "text": "autofluorescence"}, {"size": 10, "text": "visualizer"}, {"size": 32, "text": "brightness"}, {"size": 10, "text": "field-of-view"}, {"size": 11, "text": "slider"}, {"size": 457, "text": "camera"}, {"size": 23, "text": "convolutional"}, {"size": 18, "text": "grayscale"}, {"size": 10, "text": "dual-color"}, {"size": 10, "text": "side-view"}, {"size": 77, "text": "overlay"}, {"size": 21, "text": "confocal"}, {"size": 11, "text": "20x"}, {"size": 21, "text": "photographic"}, {"size": 10, "text": "micrometer"}, {"size": 10, "text": "imager"}, {"size": 106, "text": "microscopy"}, {"size": 21, "text": "digitally"}, {"size": 125, "text": "fluorescence"}, {"size": 70, "text": "imagery"}, {"size": 86, "text": "scanner"}, {"size": 16, "text": "photography"}, {"size": 10, "text": "tomographic"}, {"size": 19, "text": "eclipse"}, {"size": 10, "text": "holographic"}, {"size": 10, "text": "photographed"}, {"size": 10, "text": "microsite"}, {"size": 10, "text": "multiphoton"}, {"size": 27, "text": "bright"}, {"size": 547, "text": "imaging"}, {"size": 10, "text": "zoom-in"}, {"size": 19, "text": "image."}, {"size": 62, "text": "microscope"}, {"size": 10, "text": "lapse"}, {"size": 250, "text": "video"}, {"size": 10, "text": "in-focus"}, {"size": 10, "text": "deconvolute"}, {"size": 10, "text": "camera."}, {"size": 10, "text": "barcode-scanner"}, {"size": 10, "text": "deconvolution"}, {"size": 10, "text": "3-dimensional"}, {"size": 94, "text": "plane"}, {"size": 10, "text": "flatbed"}, {"size": 717, "text": "visualization"}, {"size": 10, "text": "captured"}, {"size": 10, "text": "greyscale"}, {"size": 10, "text": "brightest"}, {"size": 31, "text": "imagej"}, {"size": 12, "text": "epifluorescence"}, {"size": 10, "text": "scanner."}, {"size": 29, "text": "semi-automated"}, {"size": 10, "text": "stereomicroscope"}, {"size": 243, "text": "stack"}, {"size": 16, "text": "magnification"}, {"size": 10, "text": "8-bit"}, {"size": 78, "text": "scanning"}, {"size": 10, "text": "40x"}, {"size": 10, "text": "16-bit"}]
-        return render_template("wordcloud.html", wordcloud_data=wordcloud_data)
+        path_to_wordcloud = "/home/hclent/repos/citesCyverse/flask/static/wordclouds"
+        filename = "cyverseTop100.json"
+        path = os.path.join(path_to_wordcloud, filename)
+        message = "Cloud for top 200 words in all citing papers"
+        with open(path) as f:
+            wordcloud_data = str(json.load(f))
+            return render_template("wordcloud.html", wordcloud_data=wordcloud_data, message=message)
 
 
-#TODO sometimes failes cuz tag[i].startswith its being a list or something
+
+
+#TODO sometimes failes cuz tag[i].startswith its being a list or something --> problem with 2015-2017 data
 #TODO: Needs better filtering of cyverse-specific/publication stopwords ("figure", "table") etc.
 #TODO: need to decide what the bins will be here for top NPs (only the top 400 is probably not ideal)
 @app.route("/cy-embeddings/", methods=["GET","POST"])
@@ -140,7 +143,7 @@ def cyEmbeddings():
                 year_interval = '2015_2017'
                 message_years = "2015-2017"
             else:
-                year_interval = str(years) #if something funky happens just... set this to 2010-2013 :/
+                year_interval = str(years)
                 message_years = str(years)
 
             filename = 'cyverse_lemmas_' + year_interval + '.pickle'
@@ -186,9 +189,25 @@ def cyEmbeddings():
             logging.info("clustering vectors ... ")
             kmeans = KMeans(n_clusters=k_clusters, random_state=2).fit(matrix)
             results = list(zip(kmeans.labels_, top))
+
+            #filter topics
+            labels = [r[0] for r in results]
+
+            delete_topics = []
+            for i in range(0, k):
+                if labels.count(i) < 3:  # clean out topics with less than 3 words in them
+                    delete_topics.append(i)
+
+            keep_results = []
+
+            for combo in results:
+                if combo[0] not in delete_topics:
+                    keep_results.append(combo)
+
+
             #save to json
             logging.info("saving results to json ... ")
-            embedding_json(results, query, k_clusters, window, years)
+            embedding_json(keep_results, query, k_clusters, window, years)
             message =  str(k_clusters) + " topics from the top ~" + str(window) + " noun phrases from " + str(message_years)
             return render_template("embeddings.html", filepath=filepath, message=message)
 
--- flask/wordcloud.py
+++ flask/wordcloud.py
@@ -1,18 +1,12 @@
-import os, json, pickle
-from collections import defaultdict
+import os, json, pickle, nltk
+from collections import defaultdict, Counter
 from processors import *
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy import sparse
 from app import model
+#from fasttext import load_model
 
-
-
-
-#vectors_dict = "/home/hclent/repos/citesCyverse/flask/wordvecDict.pickle"
-
-
-
-
+#model = load_model("17kmodel.vec")
 
 
 #use train embeddings and do like "genetics cloud", "plant cloud", "ocean cloud", "animal cloud" ??
@@ -22,12 +16,26 @@ def flatten(listOfLists):
 
 #Step 1: Make dictionary with word counts for ALL cyverseDocs {'gluten': 5, 'span': 9}
 def frequency_dict(lemma_file):
+    #The Counter() is for if we want to get the top 100 words or w/e
+    #nesDict = Counter(defaultdict(lambda: 0)) # can't pickle a default dict, but seems fast enought o generate..
+
     nesDict = defaultdict(lambda: 0) # can't pickle a default dict, but seems fast enought o generate..
 
     cyverse_stop_words = ['university', '%', 'table', 'figure', '\\u', '\\\\', '\\', 'author', 'publication',
                           'appendix',
                           'table', 'author', 'skip', 'main', '.', 'title', 'u2009', 'publisher',
-                          'www.plantphysiol.org', 'copyright']
+                          'www.plantphysiol.org', 'copyright', '. . . .', '. .', ',', '.....', "\"", "1", ";", "3",
+                          "one", "also", "=", "2", "4" "number", 'j.', 'm.', 's.', 'many', 'b', '6', '10', 'however',
+                          'well', 'c', 'p.', '*', "'s", ':', "'", '0', '4', '-', 'three', 'may', 'non', 'could',
+                          'would', 'two', 'one',
+                          'e.g.', 'doi', 'case', 'follow', 'describe', 'name', 'see', 'among', 'single', 'several',
+                          'run', 'additional',
+                          'number', 'show', 'include', 'use', 'multiple', 'important', 'individual', 'like', 'exist',
+                          'related', 'gateway', 'control', 'suggest', "within"]
+
+    eng_stopwords = nltk.corpus.stopwords.words('english')
+    for s in eng_stopwords:
+        cyverse_stop_words.append(s)
 
     path_to_lemma_samples = "/home/hclent/repos/citesCyverse/flask/lemmas"
     full_filename = os.path.join(path_to_lemma_samples, lemma_file) #pickle
@@ -72,11 +80,12 @@ def wordcloud(query, nesDict, wordcloud_words):
         if size < 10:
             entry = {"text": word, "size": 10}
         else:
-            entry = {"text": word, "size": nesDict[word]}
+            entry = {"text": word, "size": int(nesDict[word]*0.01)} # if the size is too big (like 20,000) you MUST scale it or it will not show up in the vis
+
+
         wordcloud_list.append(entry)
 
     wordcloud_json = json.dumps(wordcloud_list)
-    #print(wordcloud_json)
 
     path_to_wordcloud = "/home/hclent/repos/citesCyverse/flask/static/wordclouds"
     filename = query + ".json"
@@ -89,8 +98,49 @@ def wordcloud(query, nesDict, wordcloud_words):
     return wordcloud_json
 
 
+
+
+
 # nesDict = frequency_dict("cyverse_lemmas_ALL.pickle")
 # wordcloud_words = filter_by_embeddings("disease", nesDict)
 # print(wordcloud_words)
 # print("#"*20)
 # wordcloud_json = wordcloud("disease", nesDict, wordcloud_words)
+
+
+
+############################################################################
+#instead of using embeddings, gonna get the top 100 words
+# nesDict = frequency_dict("cyverse_lemmas_ALL.pickle")
+# wordcloud_words = (Counter(nesDict).most_common(250))
+# cyverse_stop_words = ['university', '%', 'table', 'figure', '\\u', '\\\\', '\\', 'author', 'publication',
+#                           'appendix',
+#                           'table', 'author', 'skip', 'main', '.', 'title', 'u2009', 'publisher',
+#                           'www.plantphysiol.org', 'copyright', '. . . .', '. .', ',', '.....', "\"", "1", ";", "3",
+#                           "one", "also", "=", "2", "4" "number", 'j.', 'm.', 's.', 'many', 'b', '6', '10', 'however',
+#                           'well', 'c', 'p.', '*', "'s", ':', "'", '0', '4', '-', 'three', 'may', 'non', 'could',
+#                           'would', 'two', 'one',
+#                           'e.g.', 'doi', 'case', 'follow', 'describe', 'name', 'see', 'among', 'single', 'several',
+#                           'run', 'additional',
+#                           'number', 'show', 'include', 'use', 'multiple', 'important', 'individual', 'like', 'exist',
+#                           'related', 'gateway', 'control', 'suggest', 'high', 'allow', 'first', 'list', 'define', 'set',
+#                             'new', 'different', 'thus', 'small', 'year', 'due', 'i.e.', '...', 'low', 'per', 'big', 'via',
+#                             '. . . . .', 'full', 'another', 'second', '100'
+#                       ]
+# eng_stopwords = nltk.corpus.stopwords.words('english')
+# for s in eng_stopwords:
+#     cyverse_stop_words.append(s)
+#
+# keep_words = []
+# for w in wordcloud_words:
+#     word = w[0]
+#     if word not in cyverse_stop_words and not len(word)<3:
+#         keep_words.append(word)
+#         print(w)
+#
+# print(keep_words)
+# print(len(keep_words))
+# #
+# #
+# #
+# wordcloud_json = wordcloud("cyverseTop100", nesDict, keep_words)
